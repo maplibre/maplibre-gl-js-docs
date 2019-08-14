b74dad6c2b4292fb8cd28a1861addd8ed039bc03
Katy DeCorah <decorah@mapbox.com>
2019-08-14 14:56:19 -0400

    [docs] enable source-map (#21)

--- batfish.config.js
+++ batfish.config.js
@@ -10,6 +10,7 @@ module.exports = () => {
         outputDirectory: path.join(__dirname, '_site'),
         browserslist: mapboxAssembly.browsersList,
         postcssPlugins: mapboxAssembly.postcssPipeline.plugins,
+        productionDevtool: 'source-map',
         stylesheets: [
             require.resolve('@mapbox/mbx-assembly/dist/assembly.css'),
             require.resolve('@mapbox/dr-ui/css/docs-prose.css'),
