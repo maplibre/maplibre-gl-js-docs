8bafde9d3d7fcfd9be70117d968f0925d029c7e3
Colleen McGinnis <colleen.mcginnis@mapbox.com>
2019-09-12 12:44:10 -0700

    Add toggle renderWorldCopies example (#72)
    * add toggle renderWorldCopies example
    
    * use @asheemmamoowala's suggestion
    
    Co-Authored-By: Asheem Mamoowala <asheem.mamoowala@mapbox.com>
    
    * rename all pieces from rendered-* to render-*

---
+++ docs/pages/example/render-world-copies.js
@@ -0,0 +1,10 @@
+/*---
+title: Render world copies
+description: Toggle between rendering a single world and multiple copies of the world.
+tags:
+  - styles
+pathname: /mapbox-gl-js/example/render-world-copies/
+---*/
+import Example from '../../components/example';
+import html from './render-world-copies.html';
+export default Example(html);
