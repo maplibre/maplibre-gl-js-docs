e517f97eb10bc827509c4eaac00832513571f058
Heather Stenson <heather.stenson@mapbox.com>
2019-09-17 13:25:10 -0700

    Update icon in /mapbox-gl-js/example/popup-on-click/ (#81)
    * change icon to rocket
    
    * add maki link to description

--- docs/pages/example/popup-on-click.js
+++ docs/pages/example/popup-on-click.js
@@ -1,6 +1,6 @@
 /*---
 title: Display a popup on click
-description: 'When a user clicks a symbol, show a [`Popup`](/mapbox-gl-js/api/#popup) containing more information.'
+description: 'When a user clicks a symbol, show a [`Popup`](/mapbox-gl-js/api/#popup) containing more information. The symbols are from the [Maki](https://labs.mapbox.com/maki-icons/) symbol set used in the Mapbox Streets style.'
 tags:
   - controls-and-overlays
 pathname: /mapbox-gl-js/example/popup-on-click/
