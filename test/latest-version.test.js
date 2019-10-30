dfc6a153a32c0fa8e49ff21269fb95439198bfce
Dan Swick <dan.swick@gmail.com>
2019-10-30 13:33:09 -0700

    Add tape test to verify pinned version of GL-JS is the latest stable (#124)

---
+++ test/latest-version.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const test = require('tape');
+const path = require('path');
+const helpers = require('./helpers');
+
+let tags = [];
+
+async function tagsMatch() {
+    await process.chdir(path.join(process.cwd(), './mapbox-gl-js'));
+    const latest = await helpers.latestStableTag();
+    const current = await helpers.currentTag();
+    if (latest === current) {
+        await process.chdir(path.join(process.cwd(), '..'));
+        return true;
+    } else {
+        await process.chdir(path.join(process.cwd(), '..'));
+        tags.push(latest);
+        tags.push(current);
+        return false;
+    }
+}
+
+test(`Is latest version`, t => {
+    tagsMatch().then(res => {
+        t.ok(
+            res,
+            `Mapbox GL JS submodule is not pinned to the latest stable release version.
+             Latest: ${tags[0]}. Currently checked out: ${tags[1]}.
+             Check out the most recent with:
+             "git checkout tags/${tags[0]}"`
+        );
+        t.end();
+    });
+});
