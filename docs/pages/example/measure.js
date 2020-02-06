32709f00e5082a9ae573e67888b5a26702e63e2b
vladimir <dashukevichvova@gmail.com>
2020-02-06 09:12:57 +0300

    Update turf library and measure example. (#173)

--- docs/pages/example/measure.js
+++ docs/pages/example/measure.js
@@ -2,7 +2,7 @@
 title: Measure distances
 description: >-
   Click points on a map to create lines that measure distanced using
-  [turf.lineDistance](http://turfjs.org/static/docs/module-turf_line-distance.html).
+  [turf.length](https://turfjs.org/docs/#length).
 tags:
   - user-interaction
 pathname: /mapbox-gl-js/example/measure/
