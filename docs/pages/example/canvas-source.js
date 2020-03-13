f9a65cf66904a2b579bbc94810c70d0dca55dfa3
Mal Wood-Santoro <mal.wood@mapbox.com>
2020-03-13 12:21:42 -0700

    Add canvas source example (#230)
    * add canvas source example
    
    * spacing fix
    
    * add comment about animate: true
    
    * spacing fix
    
    * update submodule version

---
+++ docs/pages/example/canvas-source.js
@@ -0,0 +1,11 @@
+/*---
+title: Add a canvas source 
+description: Add a [canvas source](/mapbox-gl-js/api/#canvassource) to the map.
+tags:
+  - sources
+pathname: /mapbox-gl-js/example/canvas-source/
+---*/
+import Example from '../../components/example';
+import html from './canvas-source.html';
+
+export default Example(html);
