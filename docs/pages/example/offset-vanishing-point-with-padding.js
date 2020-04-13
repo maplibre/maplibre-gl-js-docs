748ee484d2ef539d65220459dd5c22ad6cf4a0a7
Arindam Bose <arindam.bose@mapbox.com>
2020-04-13 10:28:50 -0700

    Add an example for padding (#248)
    * Adding the padding example
    
    * Update tags and tweak the example a bit
    
    * Fix lint error
    
    * Simplify and reduce css
    
    * Fix typos and simplify a bit more
    
    * Update example image
    
    * Rename example so its clearer
    
    * Update tags
    
    * Run prettier
    
    * Address CR comments

---
+++ docs/pages/example/offset-vanishing-point-with-padding.js
@@ -0,0 +1,12 @@
+/*---
+title: Offset the vanishing point using padding
+description: >-
+  Offset the apparent center or [vanishing point](https://en.wikipedia.org/wiki/Vanishing_point) of the map to reduce distortion when floating elements are displayed over the map.
+tags:
+  - camera
+pathname: /mapbox-gl-js/example/offset-vanishing-point-with-padding/
+---*/
+import Example from '../../components/example';
+import html from './offset-vanishing-point-with-padding.html';
+
+export default Example(html);
