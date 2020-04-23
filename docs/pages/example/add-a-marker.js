120173780746e09b80172576187a72c572a17ea9
Adriana Babakanian <adriana.babakanian@mapbox.com>
2020-04-23 12:25:12 -0700

    Add a minimal default marker example (#220)
    * Add a minimal default marker example
    
    * Remove unused coordinates variable
    
    * Add marker to no-unused-vars exceptions

---
+++ docs/pages/example/add-a-marker.js
@@ -0,0 +1,12 @@
+/*---
+title: Add a default marker
+description: >-
+  Add a default [`Marker`](/mapbox-gl-js/api/#marker) to the map.
+tags:
+  - styles
+pathname: /mapbox-gl-js/example/add-a-marker/
+---*/
+import Example from '../../components/example';
+import html from './add-a-marker.html';
+
+export default Example(html);
