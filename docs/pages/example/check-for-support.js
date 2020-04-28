6cd1e32bc88b7a2163633efb9ee08b60ec2ef48b
Katy DeCorah <decorah@mapbox.com>
2020-04-28 07:53:37 -0400

    Reframe browser support example (#285)

--- docs/pages/example/check-for-support.js
+++ docs/pages/example/check-for-support.js
@@ -1,6 +1,6 @@
 /*---
-title: Check for browser support
-description: Check for Mapbox GL browser support.
+title: Check if Mapbox GL JS is supported
+description: Check for Mapbox GL browser support. Find more information about browser support for Mapbox tools in the [Browser support](https://docs.mapbox.com/help/troubleshooting/mapbox-browser-support/) troubleshooting guide.
 tags:
   - browser-support
 pathname: /mapbox-gl-js/example/check-for-support/
