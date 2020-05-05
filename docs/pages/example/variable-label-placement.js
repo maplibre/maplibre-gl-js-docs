368c27b2d8f0de4ab2a66d0c6e324fcaa6c13704
Dom Brassey <dom.brassey@mapbox.com>
2020-05-05 11:38:06 -0700

    add hyperlink to variable-label-placement in docs (#292)
    Addresses https://github.com/mapbox/documentation/issues/326

--- docs/pages/example/variable-label-placement.js
+++ docs/pages/example/variable-label-placement.js
@@ -1,6 +1,6 @@
 /*---
 title: Variable label placement
-description: Use `text-variable-anchor` to allow high priority labels to shift position to stay on the map.
+description: Use [`text-variable-anchor`](https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/#layout-symbol-text-variable-anchor) to allow high priority labels to shift position to stay on the map.
 tags:
   - layers
 pathname: /mapbox-gl-js/example/variable-label-placement/
