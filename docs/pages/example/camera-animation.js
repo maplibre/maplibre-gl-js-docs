8cd1add3a6e87011b477126badd0f519430fff62
Ryan Hamley <ryan.hamley@mapbox.com>
2020-05-04 19:24:01 -0700

    [docsprint] Add an example for AnimationOptions (#278)

---
+++ docs/pages/example/camera-animation.js
@@ -0,0 +1,12 @@
+/*---
+title: Customize camera animations
+description: Customize camera animations using [AnimationOptions](https://docs.mapbox.com/mapbox-gl-js/api/#animationoptions).
+tags:
+  - user-interaction
+  - camera
+pathname: /mapbox-gl-js/example/camera-animation/
+---*/
+import Example from '../../components/example';
+import html from './camera-animation.html';
+
+export default Example(html);
