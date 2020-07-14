3aa980d18eb139d0cdf6f45bc4b4e36c35f3ffed
Dan Swick <dan.swick@gmail.com>
2020-07-13 17:54:10 -0700

    Update fallback-image.js description  (#222)
    * Update fallback-image.js
    
    * Update docs/pages/example/fallback-image.js
    
    Co-authored-by: Colleen McGinnis <colleen.mcginnis@mapbox.com>
    
    Co-authored-by: Colleen McGinnis <colleen.mcginnis@mapbox.com>

--- docs/pages/example/fallback-image.js
+++ docs/pages/example/fallback-image.js
@@ -1,6 +1,6 @@
 /*---
 title: Use a fallback image
-description: Fallback to another image when a requested image is not available.
+description: Use a [coalesce](https://docs.mapbox.com/mapbox-gl-js/style-spec/expressions/#coalesce) expression to display another image when a requested image is not available.
 tags:
   - layers
 pathname: /mapbox-gl-js/example/fallback-image/
