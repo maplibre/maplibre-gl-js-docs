67e1c9cc3d3a1a7696947b66486d49aa4d215bd3
Colleen McGinnis <colleen.mcginnis@mapbox.com>
2020-08-20 15:35:26 -0700

    simplify the quickstart code (#395)

--- docs/components/quickstart.js
+++ docs/components/quickstart.js
@@ -13,7 +13,7 @@ class Quickstart extends React.Component {
     }
 
     mapOptions = `{
-    container: '<your HTML element id>',
+    container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
     center: [-74.5, 40], // starting position [lng, lat]
     zoom: 9 // starting zoom
