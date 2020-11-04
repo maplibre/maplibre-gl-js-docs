239146dfebfab14766454aa2957de8189ea2b8ca
Katy DeCorah <decorah@mapbox.com>
2020-11-04 14:48:16 -0500

    Add related section to expressions; add example title (#438)
    * Add related section to expressions; add example title
    
    * Add test to assert json shape and live links
    
    * add all existing related gl js examples to expressions-related.json
    
    * fix broken linke
    
    * limit to 3 examples for get,interpolate,round,==
    
    * limit to 3-4 examples for get,interpolate,round,==
    
    * Remove duplicated words
    
    * Add trailing slash (save the server time of adding trailing slash)
    
    * Clean up code
    
    * No more than 5 related links per property
    
    * Example heading -> Syntax
    
    * remove excess examples from 'in' expression
    
    * order 'related' items alphabetically
    
    * remove all 3 examples for round since the usage is not an expression
    
    * remove false positives
    
    Co-authored-by: Dom Brassey <dom.brassey@mapbox.com>

--- docs/components/style-spec/expression-reference.js
+++ docs/components/style-spec/expression-reference.js
@@ -6,38 +6,61 @@ import md from '../md';
 import { highlightJavascript } from '../prism_highlight';
 import { renderSignature } from './render-signature';
 import Property from './property.js';
+import related from '../../data/expressions-related.json';
 
 export default class ExpressionReference extends React.Component {
     render() {
         const group = groupedExpressions.filter(
             (g) => g.name === this.props.group
         )[0];
-        return (
-            <React.Fragment>
-                {group.expressions.map(({ name, doc, type, sdkSupport }) => (
-                    <React.Fragment key={name}>
-                        <Property
-                            id={`${
-                                group.name === 'Types' ? 'types-' : ''
-                            }${name}`}
-                        >
-                            {name}
-                        </Property>
 
-                        {doc && <div>{md(doc)}</div>}
-
-                        {type.map((overload, i) => (
-                            <div key={i}>
-                                {highlightJavascript(
-                                    renderSignature(name, overload)
-                                )}
-                            </div>
-                        ))}
-                        {sdkSupport && <SDKSupportTable {...sdkSupport} />}
-                    </React.Fragment>
-                ))}
-            </React.Fragment>
+        const SubHeading = ({ children }) => (
+            <h3
+                style={{ fontSize: '15px', lineHeight: '24px' }}
+                className="txt-bold mb6 unprose pt0"
+            >
+                {children}
+            </h3>
         );
+
+        const Related = ({ links }) => {
+            if (!links || !links.length === 0) return;
+            return (
+                <React.Fragment>
+                    <SubHeading>Related</SubHeading>
+                    <ul className="mb18">
+                        {links.map((link) => (
+                            <li key={link.title}>
+                                <a href={link.href}>{link.title}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </React.Fragment>
+            );
+        };
+        return group.expressions.map(({ name, doc, type, sdkSupport }) => (
+            <React.Fragment key={name}>
+                {/* Section heading */}
+                <Property
+                    id={`${group.name === 'Types' ? 'types-' : ''}${name}`}
+                >
+                    {name}
+                </Property>
+                {/* Description */}
+                {doc && <div className="mb12">{md(doc)}</div>}
+                {/* Syntax */}
+                <SubHeading>Syntax</SubHeading>
+                {type.map((overload, i) => (
+                    <div key={i}>
+                        {highlightJavascript(renderSignature(name, overload))}
+                    </div>
+                ))}
+                {/* Show related links if available */}
+                {related[name] && <Related links={related[name]} />}
+                {/* Show SDK support table if available */}
+                {sdkSupport && <SDKSupportTable {...sdkSupport} />}
+            </React.Fragment>
+        ));
     }
 }
 
