f054f9b4cc428cd4304459e03de7cfc437c6a37b
wipfli <mail@example.com>
2021-03-14 16:25:04 +0100

    Update helmet favicon path

--- vendor/docs-page-shell/react-page-shell.js
+++ vendor/docs-page-shell/react-page-shell.js
@@ -134,33 +134,33 @@ function PageHelmet() {
   }), React.createElement("link", {
     rel: "apple-touch-icon",
     sizes: "180x180",
-    href: "https://static-assets.mapbox.com/branding/favicon/v1/apple-touch-icon.png?v=gAd4JjrGWl"
+    href: "/maplibre-gl-js-docs/assets/favicon/apple-touch-icon.png?v=rMBbQAR5BL"
   }), React.createElement("link", {
     rel: "icon",
     type: "image/png",
     sizes: "32x32",
-    href: "https://static-assets.mapbox.com/branding/favicon/v1/favicon-32x32.png?v=gAd4JjrGWl"
+    href: "/maplibre-gl-js-docs/assets/favicon/favicon-32x32.png?v=rMBbQAR5BL"
   }), React.createElement("link", {
     rel: "icon",
     type: "image/png",
     sizes: "16x16",
-    href: "https://static-assets.mapbox.com/branding/favicon/v1/favicon-16x16.png?v=gAd4JjrGWl"
+    href: "/maplibre-gl-js-docs/assets/favicon/favicon-16x16.png?v=rMBbQAR5BL"
   }), React.createElement("link", {
     rel: "manifest",
-    href: "https://static-assets.mapbox.com/branding/favicon/v1/site.webmanifest?v=gAd4JjrGWl"
+    href: "/maplibre-gl-js-docs/assets/favicon/site.webmanifest?v=rMBbQAR5BL"
   }), React.createElement("link", {
     rel: "mask-icon",
-    href: "https://static-assets.mapbox.com/branding/favicon/v1/safari-pinned-tab.svg?v=gAd4JjrGWl",
+    href: "/maplibre-gl-js-docs/assets/favicon/safari-pinned-tab.svg?v=rMBbQAR5BL",
     color: "#4264fb"
   }), React.createElement("link", {
     rel: "shortcut icon",
-    href: "https://static-assets.mapbox.com/branding/favicon/v1/favicon.ico?v=gAd4JjrGWl"
+    href: "/maplibre-gl-js-docs/assets/favicon/favicon.ico?v=rMBbQAR5BL"
   }), React.createElement("meta", {
     name: "msapplication-TileColor",
     content: "#ffffff"
   }), React.createElement("meta", {
     name: "msapplication-config",
-    content: "https://static-assets.mapbox.com/branding/favicon/v1/browserconfig.xml?v=gAd4JjrGWl"
+    content: "/maplibre-gl-js-docs/assets/favicon/browserconfig.xml?v=rMBbQAR5BL"
   }), React.createElement("meta", {
     name: "theme-color",
     content: "#ffffff"
