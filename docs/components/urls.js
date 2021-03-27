ce496d4e688d80b2d595b88b2f1431bbba832df8
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-03-27 17:57:35 +0100

    Update submodule (#11)
    * Use maplibre in .gitmodules
    
    * Use maplibre-gl-js folder in package.json
    
    * Use maplibre in page-shell.js
    
    * Use maplibre submodule in urls.js
    
    * Use UNPKG.com and maplibre in urls.js
    
    * Increase nodejs memory max_old_space_size from 2048 to 4096

--- docs/components/urls.js
+++ docs/components/urls.js
@@ -1,11 +1,11 @@
-import { version } from '../../mapbox-gl-js/package.json';
+import { version } from '../../maplibre-gl-js/package.json';
 import { prefixUrl } from '@mapbox/batfish/modules/prefix-url';
 
 function url(ext, options) {
     if (options && options.local && process.env.DEPLOY_ENV === 'local') {
-        return prefixUrl(`/dist/mapbox-gl.${ext}`);
+        return prefixUrl(`/dist/maplibre-gl.${ext}`);
     } else {
-        return `https://api.mapbox.com/mapbox-gl-js/v${version}/mapbox-gl.${ext}`;
+        return `https://unpkg.com/maplibre-gl@${version}/dist/maplibre-gl.${ext}`;
     }
 }
 
