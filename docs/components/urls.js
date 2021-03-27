cdc7252d074569634089b3e22befb00ccc197fff
wipfli <mail@example.com>
2021-03-27 16:54:03 +0100

    Use UNPKG.com and maplibre in urls.js

--- docs/components/urls.js
+++ docs/components/urls.js
@@ -3,9 +3,9 @@ import { prefixUrl } from '@mapbox/batfish/modules/prefix-url';
 
 function url(ext, options) {
     if (options && options.local && process.env.DEPLOY_ENV === 'local') {
-        return prefixUrl(`/dist/mapbox-gl.${ext}`);
+        return prefixUrl(`/dist/maplibre-gl.${ext}`);
     } else {
-        return `https://api.mapbox.com/mapbox-gl-js/v${version}/mapbox-gl.${ext}`;
+        return `https://unpkg.com/maplibre-gl@${version}/dist/maplibre-gl.${ext}`;
     }
 }
 
