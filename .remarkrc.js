3186ae097b2f35ad5ba0e2f5008701857a1d5e82
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2022-07-17 15:18:47 +0200

    Remove remark-lint-mapbox and copyeditor dependencies (#211)

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,50 +1,6 @@
-const topicsOrder = require('./docs/data/topics.json');
-
 const config = [
-    ['@mapbox/remark-lint-link-text', [2]],
     ['remark-lint-heading-increment', [2]],
-    [
-        '@mapbox/remark-lint-mapbox/link-checker/skip-internal',
-        [
-            1,
-            {
-                siteBasePath: 'maplibre-gl-js-docs',
-                pages: 'docs/pages/',
-                constants: 'docs/constants.json',
-                ignoreLinks: 'conf/ignore-links.json'
-            }
-        ]
-    ],
-    [
-        '@mapbox/remark-lint-mapbox/link-checker/skip-external',
-        [
-            1,
-            {
-                siteBasePath: 'maplibre-gl-js-docs',
-                pages: 'docs/pages/',
-                constants: 'docs/constants.json',
-                ignoreLinks: 'conf/ignore-links.json'
-            }
-        ]
-    ],
-    [
-        '@mapbox/remark-lint-mapbox/constantly',
-        [
-            2,
-            {
-                constants: 'docs/constants.json'
-            }
-        ]
-    ],
-    ['remark-frontmatter', ['yaml']],
-    [
-        '@mapbox/remark-lint-mapbox/frontmatter',
-        {
-            disableProducts: true,
-            topicOptions: topicsOrder
-        }
-    ],
-    ['@mapbox/remark-lint-mapbox/xtreme', [2]]
+    ['remark-frontmatter', ['yaml']]
 ];
 
 exports.plugins = [...config];
