2c66fb208a4869fb9365c853dff78fb37d9fe89a
Birk Skyum <birk.skyum@pm.me>
2022-08-21 12:53:31 +0200

    add section types to throws

--- docs/components/api/throws.tsx
+++ docs/components/api/throws.tsx
@@ -21,6 +21,8 @@ export default class Throws extends React.Component<ThrowsProps> {
 }
 
 type ThrowsProps = {
-    section: any,
+    section: {
+      throws: any[]
+    },
     headingLevel?: number
 };
