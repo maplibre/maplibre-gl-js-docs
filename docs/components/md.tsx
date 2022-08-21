f62d430f965a75c5de56639865b570b7340cf2ef
Birk Skyum <birk.skyum@pm.me>
2022-08-21 13:34:38 +0200

    remove babel

--- docs/components/md.tsx
+++ docs/components/md.tsx
@@ -1,13 +1,12 @@
 import { remark } from 'remark';
 import reactRenderer from 'remark-react';
-import { unified } from 'unified';
 import rehypeParse from 'rehype-parse';
 import rehypeReact from 'rehype-react';
 import { createElement, Fragment } from 'react';
 
 export default function md(str) {
     let content;
-    unified()
+    remark()
         .use(rehypeParse, { fragment: true })
         .use(rehypeReact, { createElement, Fragment })
         .process(str)
