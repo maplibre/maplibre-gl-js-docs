1b1872f6aee95794cfba812ec924794cd9612ccb
Birk Skyum <birk.skyum@pm.me>
2023-02-25 00:29:04 +0100

    Remove submodule

--- remove-assert.js
+++ remove-assert.js
@@ -1,19 +1,6 @@
 var fs = require('fs');
 
 /* eslint-disable no-undef */
-fileName = 'maplibre-gl-js/rollup/build/tsc/src/style-spec/reference/latest.js';
-
-fs.readFile(fileName, 'utf8', function (err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    var result = data.replace(/ assert { type: 'json' }/g, '');
-
-    fs.writeFile(fileName, result, 'utf8', function (err) {
-        if (err) return console.log(err);
-    });
-});
-
 fileName =
     'node_modules/@maplibre/maplibre-gl-style-spec/tsc/src/reference/latest.js';
 
